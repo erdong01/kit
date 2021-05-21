c631afced4502f76ce0727b5e1ffbd739c007255
chenqiaojie <88155745@qq.com>
2021-05-21 21:35:15 +0800

    fix

--- db/mongoDB/mongo.go
+++ db/mongoDB/mongo.go
@@ -1,24 +1,47 @@
 package mongoDB
 
 import (
+	"fmt"
 	"github.com/erDong01/micro-kit/core"
+	"go.mongodb.org/mongo-driver/mongo"
 	"reflect"
 )
 
-func SetDatabase(d interface{}) {
+type Database struct {
+	*mongo.Database
+}
+
+func SetDatabase(d interface{}, db interface{}) {
 	refValue := reflect.ValueOf(d)
 	rType := reflect.TypeOf(d)
 	client := core.New().Mongo
+
 	if rType.Kind() == reflect.Ptr {
 		rType = rType.Elem()
 		refValue = refValue.Elem()
+		fmt.Println(refValue, 1111)
 	}
 	for i := 0; i < rType.NumField(); i++ {
 		t := rType.Field(i)
-
+		fmt.Println(t.Type.String())
 		if t.Type.String() == "*mongo.Database" {
+			fmt.Println(t.Name)
+			fmt.Println(refValue.FieldByName(t.Name), 88888888)
 			refValue.FieldByName(t.Name).
 				Set(reflect.ValueOf(client.Database(t.Name)))
+		}
+		if t.Type.String() == "*dbo.Database" {
+			dbValue := reflect.ValueOf(db)
+			fmt.Println(t.Name, 99999999)
+			dbValue.Elem().Field(0).Set(reflect.ValueOf(client.Database(t.Name)))
+			refValue.FieldByName(t.Name).Set(dbValue)
+			//refSon := reflect.TypeOf(refValue.FieldByName(t.Name).Elem())
+
+			//for i := 0; i < refSon.NumField(); i++ {
+			//	field := refSon.Field(i)
+			//	fmt.Printf("name:%s index:%d type:%v json tag:%v\n", field.Name, field.Index, field.Type, field.Tag.Get("json"))
+			//}
+			//refSon.Field(0).Set(reflect.ValueOf(client.Database(t.Name)))
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
+type Test struct {
+	Name string
+	User *User
+}
+type User struct {
+	Age  int
+	Name string
+}
+
+var test *Test
+
 func main() {
+	test = &Test{}
+	sysConfig := reflect.ValueOf(test).Elem()
+	//sysType := reflect.TypeOf(test).Elem()
+	//fmt.Println(sysConfig.FieldByIndex([]int{1})) //打印User层属性
+	//fmt.Println(sysConfig.FieldByName("Name"))
+	//fmt.Println(sysConfig.FieldByName("Age")) //直接打印下一层不成功
+	//sysConfigNew := reflect.New(sysType)
+	//sysConfigNew.Elem().FieldByName("Name").SetString("test")
+	//fmt.Println(sysConfigNew.Elem().FieldByIndex([]int{1}))
+	//fmt.Println(sysConfigNew.Elem().FieldByName("Name"))
+	//fmt.Println(test.Name)
+	user := reflect.ValueOf(&User{})
+	flag := sysConfig.FieldByName("Age") == reflect.Value{}
+	if flag {
+		fmt.Println(flag)
+		user.Elem().FieldByName("Name").SetString("testset")
+		sysConfig.Field(1).Set(user)
+		fmt.Println(sysConfig.FieldByIndex([]int{1})) //12
+	}
+	fmt.Println(test.User)
 }
