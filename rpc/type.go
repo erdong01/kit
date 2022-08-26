02479ca3075e8ca9266e527144b5d368f82c8b0f
c88155745 <88155745@qq.com>
2022-08-26 11:26:44 +0800

    no message

--- rpc/type.go
+++ rpc/type.go
@@ -1,6 +1,9 @@
 package rpc
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+)
 
 type (
 	ICluster interface {
@@ -40,3 +43,16 @@ func Route(head *RpcHead, funcName string) string {
 
 	return funcName
 }
+
+// params[0]:rpc.RpcHead
+// params[1]:error
+func Call(parmas ...interface{}) {
+	/*head := *parmas[0].(*RpcHead)
+	if parmas[1] == nil{
+		parmas[1] = ""
+	}else{
+		parmas[1] = parmas[1].(error).Error()
+	}*/
+}
+
+var GCall = reflect.ValueOf(Call)
