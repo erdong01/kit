994f0854971b8ae6615a54ca6c579bdb9206b509
c88155745 <88155745@qq.com>
2022-09-29 10:00:49 +0800

    no message

--- core/core_test.go
+++ core/core_test.go
@@ -1,15 +1,8 @@
 package core
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestBind(t *testing.T) {
-	var infoa info
-	infoa.Name = "11"
-	// New().Info = &info{}
-	Bind(&New().Info, infoa)
-	fmt.Println("111111:", New().Info.Name)
-
 }
