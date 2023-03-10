2dfbb36567a5c504867cf302da8d16df3d304ad8
c88155745 <88155745@qq.com>
2023-03-10 22:11:24 +0800

    no message

--- util/slice/slice.go
+++ util/slice/slice.go
@@ -1,32 +1,53 @@
 package slice
 
-	func Del[T comparable](s *[]T, val T) {
-		temp := *s
-		if len(temp) == 0 {
-			return
-		}
-		var i int
-		for key, v := range temp {
-			if v != val {
-				temp[i] = temp[key]
-				i++
-			}
-		}
-		*s = temp[:i]
-	}
+import (
+	"reflect"
+)
 
-	func DelByIndex[T any](s *[]T, index int) {
-		temp := *s
-		count := len(temp)
-		if count == 0 || index > count {
-			return
-		}
-		count--
-		var i int = index
-		for index < count {
-			index++
-			temp[i] = temp[index]
+func Del[T comparable](s *[]T, val T) {
+	temp := *s
+	if len(temp) == 0 {
+		return
+	}
+	var i int
+	for key, v := range temp {
+		if v != val {
+			temp[i] = temp[key]
 			i++
 		}
-		*s = temp[:i]
 	}
+	*s = temp[:i]
+
+}
+
+func DelByIndex[T any](s *[]T, index int) {
+	temp := *s
+	count := len(temp)
+	if count == 0 || index > count {
+		return
+	}
+	count--
+	var i int = index
+	for index < count {
+		index++
+		temp[i] = temp[index]
+		i++
+	}
+	*s = temp[:i]
+}
+
+func Unique[T comparable](s *[]T) {
+	var temp = *s
+	if len(*s) == 0 {
+		return
+	}
+	va := reflect.ValueOf(temp)
+	var k int = 1
+	for i := 1; i < va.Len(); i++ {
+		if !reflect.DeepEqual(va.Index(i-1).Interface(), va.Index(i).Interface()) {
+			temp[k] = temp[i]
+			k++
+		}
+	}
+	*s = temp[:k]
+}
--- util/slice/slice_test.go
+++ util/slice/slice_test.go
@@ -7,7 +7,7 @@ import (
 
 // 通过查找切片元素删除
 func TestDel(t *testing.T) {
-	var data = []int{1, 2, 3}
+	var data = []int{1, 2, 3, 4, 5}
 	Del(&data, 1)
 	fmt.Println("data", data)
 }
@@ -15,6 +15,21 @@ func TestDel(t *testing.T) {
 // 通过切片下表删除
 func TestDelByIndex(t *testing.T) {
 	var data = []int{1, 2, 3, 4, 5}
-	DelByIndex(&data, 10)
+	DelByIndex(&data, 2)
 	fmt.Println("data", data)
+}
+
+type Ball struct {
+	Name string
+	Id   int
+}
+
+func TestUnique(t *testing.T) {
+	var data = []int{1, 2, 2, 3, 3, 4, 5}
+	Unique(&data)
+	fmt.Println("data", data)
+
+	var users = []Ball{Ball{Name: "红", Id: 1}, Ball{Name: "红", Id: 1}, Ball{Name: "绿", Id: 2}, Ball{Name: "黄", Id: 3}}
+	Unique(&users)
+	fmt.Println("users", users)
 }
