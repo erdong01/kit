1bbbabaf21ede93e3d619780281983a0ce5e7efd
c88155745 <88155745@qq.com>
2023-06-07 21:56:13 +0800

    no message

--- network/WebSocket.go
+++ network/WebSocket.go
@@ -166,8 +166,8 @@ func (w *WebSocket) handleConn(tcpConn *websocket.Conn, addr string) bool {
 
 func (w *WebSocket) AddClinetJson(tcpConn *websocket.Conn, addr string, connectType int) IServerSocketClient {
 	client := &WebSocketClientJson{}
-	client.SetPacketParser(NewPacketParserJson(PacketConfig{Func: client.HandlePacketJson}))
 	client.Init("", 0)
+	client.SetPacketParser(NewPacketParserJson(PacketConfig{Func: client.HandlePacketJson}))
 	client.server = w
 	client.receiveBufferSize = w.receiveBufferSize
 	client.SetMaxPacketLen(w.GetMaxPacketLen())
