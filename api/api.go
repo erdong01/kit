7dd14b26dd9ea1e0b45bb431b7522543268fe2aa
c88155745 <88155745@qq.com>
2023-06-08 20:07:32 +0800

    no message

--- api/api.go
+++ api/api.go
@@ -1,26 +1,26 @@
 package api
 
 type Packet struct {
-	Id         uint32
-	Reply      string
-	Buff       []byte
-	JsonPacket *JsonPacket
+	Id         uint32      `json:"Id,omitempty"`
+	Reply      string      `json:"Reply,omitempty"`
+	Buff       []byte      `json:"Buff,omitempty"`
+	JsonPacket *JsonPacket `json:"JsonPacket,omitempty"`
 }
 type JsonPacket struct {
-	FuncName string
-	Head     *JsonHead
-	Data     interface{}
+	FuncName string      `json:"FuncName,omitempty"`
+	Head     *JsonHead   `json:"Head,omitempty"`
+	Data     interface{} `json:"Data,omitempty"`
 }
 type JsonHead struct {
-	Id             int64
-	SocketId       uint32
-	SrcClusterId   uint32
-	ClusterId      uint32
-	DestServerType int32
-	SendType       int32
-	ActorName      string
-	Reply          string
-	Code           int32
-	Msg            string
-	Token          string
+	Id             int64  `json:"Id,omitempty"`
+	SocketId       uint32 `json:"SocketId,omitempty"`
+	SrcClusterId   uint32 `json:"SrcClusterId,omitempty"`
+	ClusterId      uint32 `json:"ClusterId,omitempty"`
+	DestServerType int32  `json:"DestServerType,omitempty"`
+	SendType       int32  `json:"SendType,omitempty"`
+	ActorName      string `json:"ActorName,omitempty"`
+	Reply          string `json:"Reply,omitempty"`
+	Code           int32  `json:"Code,omitempty"`
+	Msg            string `json:"Msg,omitempty"`
+	Token          string `json:"Token,omitempty"`
 }
