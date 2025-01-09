9130a212396c8044d3699740fe1950fa35dffd8e
erdong01 <erdong88155745@outlook.com>
2025-01-09 14:40:35 +0800

    no message

--- httpClient/httpClient.go
+++ httpClient/httpClient.go
@@ -28,11 +28,12 @@ func (that *HttpClient) POST() (err error) {
 	}
 	if len(that.Header) > 0 {
 		req.Header = that.Header
+	} else {
+		req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1")
+		req.Header.Set("Content-Type", "application/json")
 	}
 	if that.Timeout > 0 {
 		client.Timeout = time.Duration(that.Timeout)
-	} else {
-		req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1")
 	}
 	if that.Transport != nil {
 		client.Transport = that.Transport
