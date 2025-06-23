dda1696f6731e51673a14661a627e2048e88e33b
erdong01 <erdong88155745@outlook.com>
2025-06-23 10:28:29 +0800

    fix

---
+++ test/test_worker/worker_test.go
@@ -0,0 +1,44 @@
+package testworker
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/erdong01/kit/goWorker"
+)
+
+func TestWork(t *testing.T) {
+	goworker := goWorker.NewWorker(16)
+	goworker.Go(func() {
+		fmt.Println("111")
+	})
+	goworker.Go(func() {
+		fmt.Println("222")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("333")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("444")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("555")
+	})
+	goworker.Go(func() {
+		fmt.Println("555")
+	})
+	goworker.Go(func() {
+		fmt.Println("777")
+	})
+	goworker.Go(func() {
+		fmt.Println("888")
+	})
+	goworker.Go(func() {
+		fmt.Println("999")
+	})
+
+	goworker.Wait()
+}
