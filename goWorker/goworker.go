887e68c769649f2a906d9d71038bfdbe622d03fe
erdong01 <erdong88155745@outlook.com>
2025-06-23 10:05:34 +0800

    feat:worker

---
+++ goWorker/goworker.go
@@ -0,0 +1,46 @@
+package gopool
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+	"sync"
+)
+
+type worker struct {
+	// Record the number of running workers
+	//协程数量
+	workerCount int
+	sem         chan struct{}
+	wg          sync.WaitGroup
+}
+
+func NewWorker(count ...int) (p *worker) {
+	p = &worker{}
+	p.workerCount = 32
+	if len(count) > 0 {
+		p.workerCount = count[0]
+	}
+	p.sem = make(chan struct{}, p.workerCount)
+	return p
+}
+
+func (that *worker) Go(f func()) {
+	that.sem <- struct{}{}
+	that.wg.Add(1)
+	go func() {
+		defer that.wg.Add(-1)
+		defer func() { <-that.sem }()
+		defer func() {
+			if r := recover(); r != nil {
+				msg := fmt.Sprintf("GOWorker: panic %s", debug.Stack())
+				log.Fatal(msg)
+			}
+		}()
+		f()
+	}()
+}
+
+func (that *worker) Wait() {
+	that.wg.Wait()
+}
---
+++ goWorker/goworker_test.go
@@ -0,0 +1,42 @@
+package gopool
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWork(t *testing.T) {
+	goworker := NewWorker(16)
+	goworker.Go(func() {
+		fmt.Println("111")
+	})
+	goworker.Go(func() {
+		fmt.Println("222")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("333")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("444")
+	})
+
+	goworker.Go(func() {
+		fmt.Println("555")
+	})
+	goworker.Go(func() {
+		fmt.Println("555")
+	})
+	goworker.Go(func() {
+		fmt.Println("777")
+	})
+	goworker.Go(func() {
+		fmt.Println("888")
+	})
+	goworker.Go(func() {
+		fmt.Println("999")
+	})
+
+	goworker.Wait()
+}
